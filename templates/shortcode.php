64088609676f6da47edf21ce0e4f979451d0a86f
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-07-19 11:26:32 +0200

    CHECKPOINT ==== Blocky Layout 0.0.5

--- templates/shortcode.php
+++ templates/shortcode.php
@@ -1,59 +1,77 @@
 <?php if ($docs) : ?>
 
     <div class="wedocs-shortcode-wrap">
-        <ul class="wedocs-docs-list col-<?php echo $col; ?>">
+        <ul class="wedocs-docs-list row">
             <?php foreach ($docs as $main_doc) : ?>
+                <?php if (!$main_doc['sections']) : ?>
+                    <!--Display docs with no sections-->
+                    <li class="no-section col-md-12">
+                        <ul class="row">
+                            <li class="wedocs-docs-single card col-md-12">
+                                <h2>
+                                    <a href="<?php echo get_permalink($main_doc['doc']->ID); ?>"><?php echo $main_doc['doc']->post_title; ?></a>
+                                </h2>
+                            </li>
+                        </ul>
+                    </li>
+                <?php endif;?>
+            <?php endforeach; ?>
+            <!--Display docs with sections-->
+            <li class="has-section col-md-12">
+                <ul class="row">
+                    <?php $index = 0; ?>
+                    <?php foreach ($docs as $main_doc) : ?>
+                        <?php if ($main_doc['sections']) :  ?>
+                            <?php $index++; ?>
+                            <li class="wedocs-docs-single card col">
+                                <!--IMAGE-->
+                                <?php if (has_post_thumbnail($main_doc['doc']->ID)) : ?><?php $url = wp_get_attachment_url(get_post_thumbnail_id($main_doc['doc']->ID), 'thumbnail'); ?>
+                                    <div class="row justify-content-center">
+                                        <div class="card-img-top">
+                                            <img src="<?php echo $url ?>"/>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
 
-                <!--Display only docs with sections-->
-                <?php if ($main_doc['sections']) : ?>
+                                <!--TITLE-->
+                                <h2>
+                                    <a href="<?php echo get_permalink($main_doc['doc']->ID); ?>"><?php echo $main_doc['doc']->post_title; ?></a>
+                                </h2>
 
-                    <li class="wedocs-docs-single">
-                        <h2>
-                            <a href="<?php echo get_permalink($main_doc['doc']->ID); ?>"><?php echo $main_doc['doc']->post_title; ?></a>
-                        </h2>
-                        <div class="inside container-fluid">
-                            <!--Separate sections in chunks-->
-                            <?php
-                            $chunkSize = 3;
-                            $arrayChunks = array_chunk($main_doc['sections'], $chunkSize)
-                            ?>
+                                <!--SECTIONS-->
+                                <div class="inside container-fluid">
+                                    <!--Separate sections in chunks-->
+                                    <?php
+                                    $chunkSize = 3;
+                                    $arrayChunks = array_chunk($main_doc['sections'], $chunkSize)
+                                    ?>
 
-                            <!--Print new row for each chunk-->
-                            <?php foreach ($arrayChunks as $i => $chunk) : ?>
-                                <ul class="wedocs-doc-sections row justify-content-around align-items-center">
+                                    <!--Print new row for each chunk-->
+                                    <?php //foreach ($arrayChunks as $i => $chunk) : ?>
+                                    <ul class="wedocs-doc-sections col justify-content-around align-items-center">
 
-                                    <!--Print new card for each section-->
-                                    <?php foreach ($chunk as $section) : ?>
-                                        <li class="card col-md-3">
-                                            <?php if (has_post_thumbnail($section->ID)) : ?><?php $url = wp_get_attachment_url(get_post_thumbnail_id($section->ID), 'thumbnail'); ?>
-                                                <div class="row justify-content-center">
-                                                    <div class="card-img-top">
-                                                        <img src="<?php echo $url ?>"/>
-                                                    </div>
-                                                </div>
-                                            <?php endif; ?>
-                                            <div class="card-body">
+                                        <!--Print new card for each section-->
+                                        <?php foreach ($main_doc['sections'] as $section) : ?>
+                                            <li>
                                                 <h3>
                                                     <a href="<?php echo get_permalink($section->ID); ?>"><?php echo $section->post_title; ?></a>
                                                 </h3>
-                                            </div>
-                                        </li>
-                                    <?php endforeach; ?>
+                                            </li>
+                                        <?php endforeach; ?>
 
-                                </ul>
-                            <?php endforeach; ?>
-                        </div>
-                    </li>
+                                    </ul>
+                                    <?php //endforeach; ?>
+                                </div>
+                            </li>
+                            <?php if ($index % 2 == 0) : ?>
+                </ul>
+                <ul class="row">
+                            <?php endif;?>
+                        <?php endif;?>
+                    <?php endforeach; ?>
+                </ul>
+            </li>
 
-                <!--Display docs with no sections-->
-                <?php else: ?>
-                    <li class="wedocs-docs-single">
-                        <h2>
-                            <a href="<?php echo get_permalink($main_doc['doc']->ID); ?>"><?php echo $main_doc['doc']->post_title; ?></a>
-                        </h2>
-                    </li>
-                <?php endif; ?>
-            <?php endforeach; ?>
         </ul>
     </div>
 
