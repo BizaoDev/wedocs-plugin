581b4dbcab821f2fec9705131b5d4b7b049b8ba3
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-07-19 10:34:23 +0200

    CHECKPOINT----- v.1.5 Design

--- templates/shortcode.php
+++ templates/shortcode.php
@@ -1,30 +1,60 @@
 <?php if ($docs) : ?>
 
-<div class="wedocs-shortcode-wrap">
-    <ul class="wedocs-docs-list col-<?php echo $col; ?>">
+    <div class="wedocs-shortcode-wrap">
+        <ul class="wedocs-docs-list col-<?php echo $col; ?>">
+            <?php foreach ($docs as $main_doc) : ?>
 
-        <?php foreach ($docs as $main_doc) : ?>
-            <li class="wedocs-docs-single">
-                <h2><a href="<?php echo get_permalink( $main_doc['doc']->ID ); ?>"><?php echo $main_doc['doc']->post_title; ?></a></h2>
+                <!--Display only docs with sections-->
+                <?php if ($main_doc['sections']) : ?>
 
-                <?php if ( $main_doc['sections'] ) : ?>
+                    <li class="wedocs-docs-single">
+                        <h2>
+                            <a href="<?php echo get_permalink($main_doc['doc']->ID); ?>"><?php echo $main_doc['doc']->post_title; ?></a>
+                        </h2>
+                        <div class="inside container-fluid">
+                            <!--Separate sections in chunks-->
+                            <?php
+                            $chunkSize = 3;
+                            $arrayChunks = array_chunk($main_doc['sections'], $chunkSize)
+                            ?>
 
-                    <div class="inside">
-                        <ul class="wedocs-doc-sections">
-                            <?php foreach ($main_doc['sections'] as $section) : ?>
-                                <li><a href="<?php echo get_permalink( $section->ID ); ?>"><?php echo $section->post_title; ?></a></li>
+                            <!--Print new row for each chunk-->
+                            <?php foreach ($arrayChunks as $i => $chunk) : ?>
+                                <ul class="wedocs-doc-sections row justify-content-around align-items-center">
+
+                                    <!--Print new card for each section-->
+                                    <?php foreach ($chunk as $section) : ?>
+                                        <li class="card col-md-3">
+                                            <?php if (has_post_thumbnail($section->ID)) : ?><?php $url = wp_get_attachment_url(get_post_thumbnail_id($section->ID), 'thumbnail'); ?>
+                                                <div class="row justify-content-center">
+                                                    <div class="card-img-top">
+                                                        <img src="<?php echo $url ?>"/>
+                                                    </div>
+                                                </div>
+                                            <?php endif; ?>
+                                            <div class="card-body">
+                                                <h3>
+                                                    <a href="<?php echo get_permalink($section->ID); ?>"><?php echo $section->post_title; ?></a>
+                                                </h3>
+                                            </div>
+                                        </li>
+                                    <?php endforeach; ?>
+
+                                </ul>
                             <?php endforeach; ?>
-                        </ul>
-                    </div>
+                        </div>
+                    </li>
 
+                <!--Display docs with no sections-->
+                <?php else: ?>
+                    <li class="wedocs-docs-single">
+                        <h2>
+                            <a href="<?php echo get_permalink($main_doc['doc']->ID); ?>"><?php echo $main_doc['doc']->post_title; ?></a>
+                        </h2>
+                    </li>
                 <?php endif; ?>
-
-                <div class="wedocs-doc-link">
-                    <a href="<?php echo get_permalink( $main_doc['doc']->ID ); ?>"><?php echo $more; ?></a>
-                </div>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</div>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 
 <?php endif; ?>
